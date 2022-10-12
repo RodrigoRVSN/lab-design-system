498579f3464f42474cac0c2ff16bc9ada7e22e2f
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-10-12 17:23:02 -0300

    feat: add first component Text in Storybook with variations

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,37 @@
+import { Meta, StoryObj } from '@storybook/react' 
+import { Text, TextProps } from "./Text";
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  args: {
+    children: 'Lorem Ipsum',
+    size: 'md'
+  }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+}
+
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>
+        Fala galera
+      </p>
+    )
+  }
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,8 +7,28 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif', 
-      }
+      },
     },
+    
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+
+      'cyan-500': '#81D0F7',
+      'cyan-300': '#9BE1FB',
+    }
   },
   plugins: [],
 }
